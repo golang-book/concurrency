11c2efbdb551e04b86840ccd04df45e4c1b3b276
Caleb Doxsey <caleb@doxsey.net>
2019-03-16 06:58:00 -0600

    python

---
+++ python/sync.py
@@ -0,0 +1,26 @@
+def go(callback, *args):
+    pass
+
+def make(capacity=0):
+    pass
+
+def len(channel):
+    pass
+
+def cap(channel):
+    pass
+
+def send(channel, value, callback):
+    pass
+
+def recv(channel, callback):
+    pass
+
+def close(channel):
+    pass
+
+def select(cases, callback=None):
+    pass
+
+def run():
+    pass
